47cdf8fd5901f9ca28e92a02d4956304b866bd39
Patrimonio202 <149069682+Patrimonio202@users.noreply.github.com>
2023-12-31 10:14:22 -0500

    se importa el modelo item

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from typing import Union
-from pydantic import BaseModel
+
+from models.item_model import Item
+
 
 #creacion de una aplicacion
 app = FastAPI()
 
-class Item(BaseModel):
-    name:str
-    price: float
-    is_offer:Union[bool, None]= None
+
 
 @app.get("/")
 def read_root():
@@ -26,3 +25,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/calculadora")
 def calcular(operando_1: float, operando_2:float):
     return {'suma':operando_1+operando_2 }
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    return {'item_name': item.name, 'item_id': item_id}
